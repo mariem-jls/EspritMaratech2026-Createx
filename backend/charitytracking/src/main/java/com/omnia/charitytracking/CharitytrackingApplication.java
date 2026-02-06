1054604cbe3733902792c42f2f325318e1c12c0d
Ameni Mensi <ameni.mensi@esprit.tn>
2026-02-06 21:10:50 +0100

    spring boot

---
+++ backend/charitytracking/src/main/java/com/omnia/charitytracking/CharitytrackingApplication.java
@@ -0,0 +1,13 @@
+package com.omnia.charitytracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitytrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharitytrackingApplication.class, args);
+	}
+
+}
